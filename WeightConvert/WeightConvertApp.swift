cb5e0e0e054aa0730b0c1896c890a614bc7eb18c
Mykola Chaikovskyi <nickchaywork@gmail.com>
2024-08-23 12:44:22 +0300

    Initial Commit

---
+++ WeightConvert/WeightConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeightConvertApp.swift
+//  WeightConvert
+//
+//  Created by Mykola Chaikovskyi on 23.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeightConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
